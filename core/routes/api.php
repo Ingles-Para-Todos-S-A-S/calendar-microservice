163802cab13fb1fdfd0984a2f00c7aa29c53b755
Jose Nieto <jose.nieto@inglesparatodos.edu.co>
2022-11-17 16:01:30 -0500

    refactor addEventPrueba

--- core/routes/api.php
+++ core/routes/api.php
@@ -25,7 +25,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('get/event/calendar/idcalendar/all/{idcalendar}',          [CalendarController::class, 'getEventCalendarByIdCalendar']);
 Route::post('post/event/calendar/idcalendar/by/datetime',             [CalendarController::class, 'getEventCalendarByDate']);
 Route::post('post/event/create/query',                                [CalendarController::class, 'getCreateQueryEvent']);
-Route::post('post/add/event/calendar/by/daytime',                     [CalendarController::class, 'addEventCalendarByDayTim']);
+Route::post('post/add/event/calendar/by/daytime',                     [CalendarController::class, 'addEventCalendarByDayTime']);
 Route::get('get/add/event/calendar/all/day',                          [CalendarController::class, 'addEventCalendarAllDay']);
 Route::put('put/update/event/calendar/by/idevent/{idevent}',          [CalendarController::class, 'updateEventCalendarByIdEvent']);
 Route::get('get/delete/event/calendar/by/idevent/{idevent}',          [CalendarController::class, 'deleteEventCalendarByIdEvent']);
